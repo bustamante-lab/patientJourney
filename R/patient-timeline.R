722cd109b9a5e12e5157987588dced4412a12681
arturolp <arturolp@gmail.com>
2017-10-02 16:08:37 -0700

    First version of the package, all functions working

--- R/patient-timeline.R
+++ R/patient-timeline.R
@@ -1,153 +1,158 @@
 ###############################################################
 #
-# Project: Patient Timeline
-# Author: Arturo Lopez Pineda <arturolp@stanford.edu>
+# Package: PatientTrajectories
 # Date: Oct 02, 2017
 #
 ###############################################################
 
-library(ggplot2)
-library(gridExtra)
+require(ggplot2)
+require(gridExtra)
 
+events <- read.csv("data/event-code-patient-time-example.csv")
+umls <- read.csv("data/UMLS-codes-descriptions.csv", row.names = 1)
 
-mycolors <-
-  c(
-    "lightseagreen",
-    "lightblue",
-    "plum2",
-    "darksalmon",
-    "papayawhip",
-    "hotpink",
-    "gold1",
-    "deepskyblue",
-    "tomato",
-    "olivedrab1"
-  )
-mycodes <-
-  c(
-    "Visit",
-    "Oncology visit",
-    "Request lab",
-    "Biopsy / Pathology",
-    "Chemotherapy",
-    "Radiation / Surgery",
-    "IHC results",
-    "Treatment adjusted",
-    "Hospitalization / Metastasis",
-    "Remission"
-  )
+#------------------------
+#' Create a plot of cohort timelines
+#'
+#' @param events A dataframe of events for the cohort
+#' @param umls A dataframe of umls codes, groupings and colors
+#' @return plot
+#' @example
+#' cohortTimeline(events, umls)
+#'
+cohortTimeline <- function(events, umls) {
+  patients <- unique(events$PatientID)
 
-events = read.csv("data/event-code-patient-time-example.csv")
-umls = read.csv("data/UMLS-codes-descriptions.csv", row.names=1)
-
-#--------------------------------------
-# Step 0. Define functions
-#--------------------------------------
-patientsTimelines <- function(events, umls) {
-
-  patients = unique(events$PatientID)
-
+  plot.list <- list()
   for (patient in patients) {
     #Obtain only the indexes for a particular patient
-    patient.indexes = which(events$PatientID == patient)
-    single.patient = events[patient.indexes,]
+    patient.indexes <- which(events$PatientID == patient)
+    single.patient <- events[patient.indexes, ]
 
     #Calculate the timeline rows (one row is one year)
-    pat = calculateTimeline(single.patient, umls)
-    plotTimeline(pat, patient, mycolors, mycodes)
-
+    pat <- calculateTimeline(single.patient, umls)
+    plot.list <- c(plot.list, list(patientTimeline(pat)))
   }
 
-  #Combine all patients
+  #Create the grid of plots
+  h <- do.call(grid.arrange, plot.list)
 
+  return(h)
 }
 
+
+
+#------------------------
+#' Calculate the span distances for each event in the plot
+#'
+#' @param events A dataframe of events for a single patient
+#' @param umls A dataframe of umls codes, groupings and colors
+#' @return plot
+#' @example
+#' calculateTimeline(events, umls)
+#'
 calculateTimeline <- function(patient, umls) {
-  patient$year.label = paste("year", ceiling(patient$Time_days / 365), sep = " ")
-  patient$year = ceiling(patient$Time_days / 365)
-  patient$color = umls[as.character(patient$UMLScode), "color"]
+  patient$year.label <-
+    paste("year", ceiling(patient$Time_days / 365), sep = " ")
+  patient$year <- ceiling(patient$Time_days / 365)
+  patient$color <- umls[as.character(patient$UMLScode), "color"]
 
   #order by time, adding start and end of segment
-  patient = patient[order(patient$Time_days), ]
+  patient <- patient[order(patient$Time_days),]
 
-  start = c()
-  end = c()
-  time.start = 0
-  time.end = 0
+  start <- c()
+  end <- c()
+  time.start <- 0
+  time.end <- 0
   for (i in 1:length(patient$Time_days)) {
-    j = i
+    j <- i
     #In case two events occur at the same time
     while ((patient[j, "Time_days"] == patient[j - 1, "Time_days"]) &&
            (j > 1)) {
-      j = j - 1
+      j <- j - 1
     }
 
     if (j == 1) {
-      time.start = 0
+      time.start <- 0
     }
     else{
       if (patient[j, "year"] == patient[j - 1, "year"])
-        time.start = patient[j - 1, "Time_days"] + 1
+        time.start <- patient[j - 1, "Time_days"] + 1
       else{
-        time.start = 0
+        time.start <- 0
       }
     }
-    time.end = patient[i, "Time_days"]
+    time.end <- patient[i, "Time_days"]
 
 
     # change all values to 1-year format
     while (time.start > 365) {
-      time.start = time.start - 365
+      time.start <- time.start - 365
     }
 
     while (time.end > 365) {
-      time.end = time.end - 365
+      time.end <- time.end - 365
     }
 
-    start = c(start, time.start)
-    end = c(end, time.end)
+    start <- c(start, time.start)
+    end <- c(end, time.end)
   }
-  patient$start = start
-  patient$end = end
+  patient$start <- start
+  patient$end <- end
 
   # Add next year's event to current year for completion
-  pat2 = patient
-  j = 1
+  pat2 <- patient
+  j <- 1
   for (i in 1:(dim(patient)[1] - 1)) {
     if (patient[i, "year"] != patient[i + 1, "year"]) {
       #when the year difference is more than 1
       for (y in patient[i, "year"]:(patient[i + 1, "year"] - 1)) {
         #Event to be added
-        pevent = patient[(i + 1), ]
-        pevent$end = 365
+        pevent <- patient[(i + 1),]
+        pevent$end <- 365
         if (y == patient[i, "year"]) {
-          pevent$start = patient$end[i] + 1
+          pevent$start <- patient$end[i] + 1
         }
         else{
-          pevent$start = 0
+          pevent$start <- 0
         }
-        pevent$year = y
-        pevent$year.label = paste("year", y, sep = " ")
-        pat2 = rbind(pat2[1:j, ], pevent , pat2[(j + 1):dim(pat2)[1], ])
-        j = j + 1
+        pevent$year <- y
+        pevent$year.label <- paste("year", y, sep = " ")
+        pat2 <-
+          rbind(pat2[1:j,], pevent , pat2[(j + 1):dim(pat2)[1],])
+        j <- j + 1
       }
     }
-    j = j + 1
+    j <- j + 1
   }
 
   return(pat2)
 }
 
-#------------------------
-# plot the patient timeline
-#------------------------
-plotTimeline <- function(patient, patientID, mycolors, mycodes) {
-  years = unique(patient$year.label)
-  years = years[order(years, decreasing=TRUE)]
-  #years = c("year 4", "year 3", "year 2", "year 1")
 
+#------------------------
+#' Create a plot for a single patient
+#'
+#' @param segments A dataframe with patient information and calculated segments
+#' @return plot
+#' @example
+#' patientTimeline(events, umls)
+#'
+patientTimeline <- function(segments) {
 
-  g <- ggplot(patient) +
+  #Unique labels for years
+  years <- unique(segments$year.label)
+  years <- years[order(years, decreasing = TRUE)]
+
+  #Get patient ID
+  patientID = unique(segments[,1])
+
+  #Get legend labels and colors
+  mycolors = as.vector(segments[, "color"])
+  mycodes = as.vector(segments[, "UMLScode"])
+
+  #Create the ggplot object
+  g <- ggplot(segments) +
     geom_segment(
       aes(
         x = start,
--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# PatientTrajectories
-An R package to analyze patient's trajectories in the Electronic Medical Record
+# PatientTrajectory
+An R package to analyze patient's trajectories in the Electronic Health Record
 
 
 ## Description
-Medical trajectories of the cohort with IHC testing. Each patient is represented by a rectangle, with rows representing each year of follow-up treatment. Events are color-coded according to the type of event, and the length of the bar represents the duration between the occurrence of an event and the event that preceded it
+Medical trajectories of the cohort: Each patient is represented by a rectangle, with rows representing each year of follow-up treatment. Events are color-coded according to the type of event, and the length of the bar represents the duration between the occurrence of an event and the event that preceded it.
 
 ## Citation
 For citation and more information refer to:
 
->A.L. Pineda et al. (2017). "Therapeutic importance of timely immunophenotyping of breast cancer in a resource-constrained setting: a retrospective hospital-based cohort study".
+>A.L. Pineda et al. "Therapeutic importance of timely immunophenotyping of breast cancer in a resource-constrained setting: a retrospective hospital-based cohort study" (under review by journal).
 
 
 ## Current development status
